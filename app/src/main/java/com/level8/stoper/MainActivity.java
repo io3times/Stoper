038343ecc1e182f042b0e6e2cbac062655ba66a3
tomaszkieblesz <tomasz.kieblesz@gmail.com>
2017-01-16 19:18:58 +0100

    Pomyślałem o małej zmianie

--- app/src/main/java/com/level8/stoper/MainActivity.java
+++ app/src/main/java/com/level8/stoper/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         runTimer();
+        //Wydaje mi się to błędne
     }
     public void onStartButtonClicked(View view) {
         running = true;
