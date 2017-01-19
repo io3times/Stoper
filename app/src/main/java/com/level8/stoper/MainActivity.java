dffb3c46806f15394321bd95687c2c4d6dc0c049
Level8 <tomasz.kieblesz@gmail.com>
2017-01-19 18:47:38 +0100

    Sporo nowych aktywnsci

--- app/src/main/java/com/level8/stoper/MainActivity.java
+++ app/src/main/java/com/level8/stoper/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     private boolean running;
     private int second = 0;
+    private boolean wasRunning=false;
 
 
     @Override
@@ -18,18 +19,34 @@ public class MainActivity extends Activity {
         if(savedInstanceState!=null){
             second=savedInstanceState.getInt("second");
             running=savedInstanceState.getBoolean("running");
+            wasRunning=savedInstanceState.getBoolean("wasRunning");
         }
         runTimer();
 
     }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        running=false;
+
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+       if (wasRunning){
+           running=true;
+       } else {
+           running=false;
+       }
+    }
     public void onStartButtonClicked(View view) {
         running = true;
-
+        wasRunning=true;
     }
 
     public void onStopButtonClicked(View view) {
         running = false;
-
+        wasRunning=false;
     }
 
     public void onResetButtonClicked(View view) {
@@ -47,11 +64,13 @@ public class MainActivity extends Activity {
                 int god=second/3600;
                 int min=(second%3600)/60;
                 int sek=second%60;
+
                 String msg= String.format("%d:%02d:%02d", god,min,sek);
                 godView.setText(msg);
 
                 if (running) {
                     second++;
+
                 }
                 handler.postDelayed(this, 1000);
             }
@@ -66,6 +85,7 @@ public class MainActivity extends Activity {
     public void onSaveInstanceState(Bundle savedInstanceState){
         savedInstanceState.putInt("second", second);
         savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
     }
 
 
